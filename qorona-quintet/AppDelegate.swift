9f470b94333fa035c1bfefc487f11f1a6ea99739
Kevin Chen <kchen@palantir.com>
2020-05-31 15:05:34 -0700

    Initialize new Cocoa app

---
+++ qorona-quintet/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  qorona-quintet
+//
+//  Created by Kevin Chen on 5/31/20.
+//  Copyright © 2020 Kevin Chen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
