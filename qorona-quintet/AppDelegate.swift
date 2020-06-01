989f6b8789f12188a0d82113eb4eb5677871e78b
Kevin Chen <kchen@palantir.com>
2020-05-31 20:58:33 -0700

    test whether simple script can run on other CPUs

--- qorona-quintet/AppDelegate.swift
+++ qorona-quintet/AppDelegate.swift
@@ -7,21 +7,33 @@
 //
 
 import Cocoa
+import os.log
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     @IBOutlet weak var window: NSWindow!
-
-
+    
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
+        let helloScript = """
+tell me
+    activate
+    display dialog "Hello!"
+end tell
+"""
 
+        let script = NSAppleScript(source: helloScript)!
+        var error: NSDictionary? = nil
+        script.executeAndReturnError(&error)
+        if (error != nil) {
+            os_log("%@", error!)
+        }
+        
+        NSApplication.shared.terminate(self)
+    }
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
-
